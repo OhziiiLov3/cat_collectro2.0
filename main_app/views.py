585570251289339794872f6a5d571946412110a3
ohziiilov3 <klbaskerville0520@gmail.com>
2023-11-14 11:08:20 -0800

    chore: detail view, url working

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from .models import Cat
+
 # Create your views here.
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -15,7 +17,13 @@ def about(request):
 
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {
         'cats': cats
     });
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request,'cats/detail.html',{'cat': cat})
+
 
